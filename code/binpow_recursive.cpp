6a5ad59baf536193d613f64a763dd03f1381f048
Liubomyr Piadyk <liubomyr.piadyk@gmail.com>
2021-01-04 15:11:30 +0100

    Translate binary_pow

---
+++ code/binpow_recursive.cpp
@@ -0,0 +1,10 @@
+int binpow(int a, int n) {
+    if (n == 0)
+        return 1;
+    if (n % 2 == 1)
+        return binpow (a, n-1) * a;
+    else {
+        int b = binpow (a, n/2);
+        return b * b;
+    }
+}
